a371e8daaa2949b39b43de3105388efd7c038038
Jason <jason@jason-asus>
2020-05-01 10:37:50 -0500

    ejemplo llamado de componentes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      valor: "valor inicial",
+    };
+    // this.cambio = this.cambio.bind(this); //de esta forma uno el this interno dela funcion con el this
+  } //del constructor o documento
+
+  render() {
+    return (
+      <div>
+        <p>mi primera aplicacion en react{console.log("hola")}</p>
+        <input type='text' onChange={this.cambio} />
+        <br />
+        <button onClick={this.mostrar}>Mostrar</button>
+      </div>
+    );
+  }
+  cambio = (event) => {
+    console.log(this.state.valor); //captura el valor del cambio y lo muestra
+  };
+  mostrar = (event) => {};
 }
 
 export default App;
