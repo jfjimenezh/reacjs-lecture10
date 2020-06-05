3a2a920de717ab48d1ff0a77e6cdb8aabe5f047a
Andrea Ariza <asarizaa@libertadores.edu.co>
2020-06-05 02:01:02 -0500

    modificaciones de estilos

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,25 +4,61 @@ import { Link } from "react-router-dom";
 
 function Home(props) {
   return (
-    <div>
-      <h1 className='titulo'>
-        {
-          "Bienvenido aquí podras registrarte, consultar la  información y modificarla"
-        }
-      </h1>
-      <ol className='links'>
-        <li>
+    <div className='mainDiv'>
+      <h1 className='titulo'>{"Bienvenido aquí podras registrarte, consultar la  información y modificarla"}</h1>
+      <br />
+      <table className='links'>
+        <li className='letra'>
           <Link to='/singIn'>{"Click para modificar algun usuario"}</Link>
         </li>
-        <li>
-          <Link to='/singUp'>{"Por favor registrese Aqui"}</Link>
+        <br />
+        <li className='letra'>
+          <Link to='/singUp'>{"Por favor registrese Aquí"}</Link>
         </li>
-        <li>
+        <br />
+        <li className='letra'>
           <Link to='/verUsuarios'>
             {"Click para ver los usuarios Registrados"}
           </Link>
         </li>
-      </ol>
+        <br />
+      </table>
+      <br />    
+      <p className='p'>
+        {
+          "ELABORADO POR:"
+        }
+      </p>
+      <p className='pa'>
+        {
+          "Andrea Ariza asarizaa@libertadores.edu.co"
+        }
+      </p>
+      <p className='pa'>
+        {
+          "David Mena demenag@libertadores.edu.co"
+        }
+      </p>
+      <p className='pa'>
+        {
+          "Jason Jimenez jfjimenezh@libertadores.edu.co"
+        }
+      </p>
+      <p className='p'>
+        {
+          "REPOSITORIOS:"
+        }
+      </p>
+      <p className='pa'>
+        {
+          "FRONT: https://github.com/jfjimenezh/reacjs-lecture10"
+        }
+      </p>
+      <p className='pa'>
+        {
+          "BACK: https://github.com/jfjimenezh/backend"
+        }
+      </p>
     </div>
   );
 }
